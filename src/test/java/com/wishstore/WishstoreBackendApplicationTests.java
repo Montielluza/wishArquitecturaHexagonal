2cd6844f06d66a13a3061d0faee4cc0576243ea5
Luza Montiel <luzamontiel20@gmail.com>
2026-08-05 10:56:25 -0500

    first commit

---
+++ src/test/java/com/wishstore/WishstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
